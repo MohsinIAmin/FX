21ff2293d336b5fb9d8c68d5ba4bd7a29e0a69d4
Mohsin I Amin <48616524+MohsinIAmin@users.noreply.github.com>
2020-01-13 15:47:37 +0600

    Add files via upload

--- Login/src/login/HorijantalBox.java
+++ Login/src/login/HorijantalBox.java
@@ -3,14 +3,16 @@ package login;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 
 public class HorijantalBox {
 
     HBox box1;
-    HBox box2;
     Label label1;
     Label label2;
     TextField text;
@@ -18,34 +20,70 @@ public class HorijantalBox {
     Button btn2;
     Button btn3;
     Insets insets = new Insets(20, 20, 10, 40);
+    ChoiceBox choiceBox;
+    TextArea textArea;
+    CheckBox[] checkBox = new CheckBox[2];
 
     public HorijantalBox(String strLabel1, String strText, String strLabel2) {
         label1 = new Label(strLabel1);
         label2 = new Label(strLabel2);
         text = new TextField(strText);
         box1 = new HBox(20, label1, text, label2);
-        //box1.setAlignment(Pos.TOP_LEFT);
-        //box1.setPadding(new Insets(15, 12, 10, 180));
         box1.setPadding(insets);
         box1.setAlignment(Pos.CENTER);
     }
 
-    public HorijantalBox(int num ,String strBtn1, String strBtn2, String strBtn3) {
+    public HorijantalBox(double btnSize ,String strBtn1, String strBtn2, String strBtn3) {
         btn1 = new Button(strBtn1);
         btn2 = new Button(strBtn2);
         btn3 = new Button(strBtn3);
-        box2 = new HBox(40,btn1,btn2,btn3);
-        box2.setPadding(insets);
-        box2.setAlignment(Pos.CENTER);
+        btn1.setMinWidth(btnSize);
+        btn2.setMinWidth(btnSize);
+        btn3.setMinWidth(btnSize);
+        box1 = new HBox(40,btn1,btn2,btn3);
+        box1.setPadding(insets);
+        box1.setAlignment(Pos.CENTER);
         
     }
 
-    public HBox getHBox() {
+    public HorijantalBox(String[] choice) {
+        this.label1 = new Label("Batch : ");
+        this.label2 = new Label("    ");
+        this.choiceBox = new ChoiceBox();
+        for(int i=0;i<choice.length;i++){
+            this.choiceBox.getItems().add(choice[i]);
+        }
+        this.choiceBox.setValue(choice[0]);
+        this.box1 = new HBox(40,this.label1,this.choiceBox,this.label2);
+        box1.setPadding(insets);
+        box1.setAlignment(Pos.CENTER);
+    }
+
+    public HorijantalBox(String strLabel) {
+        this.label1 = new Label(strLabel);
+        this.textArea = new TextArea();
+        this.textArea.setScaleX(.9);
+        this.textArea.setScaleY(1);
+        this.textArea.setWrapText(true);
+        this.box1 = new HBox(40,this.label1,this.textArea);
+        box1.setPadding(insets);
+        box1.setAlignment(Pos.TOP_CENTER);
+    }
+
+    public HorijantalBox(String strLabel,String[] options) {
+        this.label1 = new Label(strLabel);
+        this.label2 = new Label();
+        this.box1 = new HBox(40,this.label1);
+        for(int i=0;i<options.length;i++){
+            this.checkBox[i] = new CheckBox(options[i]);
+            this.box1.getChildren().add(this.checkBox[i]);
+        }
+        this.box1.getChildren().add(this.label2);
+        box1.setPadding(insets);
+        box1.setAlignment(Pos.TOP_CENTER);
+    }
+    
+    public HBox getHBox1() {
         return box1;
     }
-
-    public HBox getHBox2() {
-        return box2;
-    }
-
 }
--- Login/src/login/Login.java
+++ Login/src/login/Login.java
@@ -1,63 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package login;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-/**
- *
- * @author mohsin
- */
 public class Login extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
         VBox parent = new VBox();
-        root.getChildren().add(btn);
         HorijantalBox hbox1 = new HorijantalBox("Name : ","","");
         HorijantalBox hbox2 = new HorijantalBox("Email : ","","");
-        HorijantalBox hbox3 = new HorijantalBox(3,"Email : ","","");
-        parent.getChildren().add(hbox1.getHBox());
-        parent.getChildren().add(hbox2.getHBox());
-        parent.getChildren().add(hbox3.getHBox2());
-        parent.setSpacing(30);
+        HorijantalBox hbox3 = new HorijantalBox(60,"OK","Cancel","Clear");
+        String[] choice = new String[]{"Select One","BSSE12","BSSE11","BSSE10","BSSE09"};
+        HorijantalBox hbox4 = new HorijantalBox(choice);
+        HorijantalBox hbox5 = new HorijantalBox("About yourself : ");
+        String[] options = new String[]{" Male","Female"};
+        HorijantalBox hbox6 = new HorijantalBox("Gender : ",options);
+        parent.getChildren().add(hbox1.getHBox1());
+        parent.getChildren().add(hbox2.getHBox1());
+        parent.getChildren().add(hbox6.getHBox1());
+        parent.getChildren().add(hbox4.getHBox1());
+        parent.getChildren().add(hbox5.getHBox1());
+        parent.getChildren().add(hbox3.getHBox1());
+        parent.setSpacing(2);
+        parent.setAlignment(Pos.CENTER);
         Scene scene;
-        scene = new Scene(parent, 500, 500);
+        scene = new Scene(parent, 700, 600);
 
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         launch(args);
     }
-
+    
 }
